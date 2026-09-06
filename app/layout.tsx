faa2bc0684b1686962d3c898916e18227960889a
Nalyy <ilanjue201293@gmail.com>
2026-09-07 00:02:44 +0200

    Load v5 UI polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./enhancements.css";
 import "./mobile-keyboard.css";
 import "./comms-v2.css";
 import "./chat-v4.css";
+import "./ui-v5.css";
 
 export const metadata: Metadata = {
   title: "Manuel Pro",
